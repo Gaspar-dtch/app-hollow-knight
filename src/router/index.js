3d2e7b6a0b935dbcd9ad3c86d4893e4a5ff238c0
Gaspar <gaspar.deutsch@gmail.com>
2026-02-25 17:00:33 +0100

    create route for build, map & detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Progression from '../views/Progression.vue'
+import Build from '../views/Build.vue'
+import Map from '../views/Map.vue'
+import Detail from '../views/Detail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'progression',
+      component: Progression,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/build',
+      name: 'build',
+      component: Build,
+    },
+    {
+      path: '/map',
+      name: '/map',
+      component: Map,
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: Detail,
     },
   ],
 })
