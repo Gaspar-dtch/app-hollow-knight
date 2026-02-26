211ad3e063ecd79f55a1220907d18b0fd243833f
Gaspar <gaspar.deutsch@gmail.com>
2026-02-26 14:16:34 +0100

    edit Progression

--- src/views/Progression.vue
+++ src/views/Progression.vue
@@ -1,18 +1,6 @@
 <script setup>
-import { store } from '@/stores/counter';
+import { store } from '@/stores';
 import TheWelcome from '../components/TheWelcome.vue'
-
-console.log(store.nail.map(n => n.name));
-
-
-
-// if (store.nail.map(n => n.name) !== "Vieux aiguillon"){
-  
-// }
-
-const nameNail = store.nail.slice(1).map(n => n.name)
-const nameNailArt = store['nail-arts'].map(n => n.name)
-const nameSpells = store.spells.map(n => n.name)
 
 
 </script>
@@ -21,38 +9,13 @@ const nameSpells = store.spells.map(n => n.name)
   <main>
     <h1>Progression</h1>
 
-    <div class="categories">
-      <h2>Aiguillon 4%</h2>
-
-
-      <div class="cate-items">
-        <p v-for="name in nameNail">{{ name }}</p>
-      </div>
-    </div>
-
-    <div class="categories">
-      <h2>Art de l'Aiguillon 3%</h2>
+    <div v-for="cat in store" class="categories">
+      <h2 >{{ cat.catName }}</h2>
 
       <div class="cate-items">
-        <p v-for="name in nameNailArt">{{ name }}</p>
+        <ul v-for="item in cat.items">{{ item.name }}</ul>
       </div>
     </div>
-
-    <div class="categories">
-      <h2>Sorts 6%</h2>
-
-      <div class="cate-items">
-        <p v-for="name in nameSpells">{{ name }}</p>
-      </div>
-    </div>
-
-    <div class="categories">
-      <h2>Sorts 6%</h2>
-
-      <div class="cate-items">
-        <p v-for="name in nameSpells">{{ name }}</p>
-      </div>
-    </div>
-
+    
   </main>
 </template>
